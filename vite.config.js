c479219c0004c7cf39dfe7a43633a85c0329b833
Gancho <huliganmaian@gmail.com>
2026-04-09 23:00:47 +0200

    Ajustes de las rutas de Mercat v1.0.1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+  // 'base' hace que las rutas de los archivos sean relativas (./) en lugar de absolutas (/)
+  // Esto arregla la pantalla en blanco o la rotura de Javascript en GitHub Pages.
+  base: './',
   plugins: [
     // Este plugin nos genera un certificado HTTPS temporal ('Localhost' y IPs Locales).
     // Es obligatorio para que sistemas móviles como iOS o Android no bloqueen la cámara.
