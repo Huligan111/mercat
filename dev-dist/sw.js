44614bd66d17817dab6457816541cee8d0a44de8
Gancho <huliganmaian@gmail.com>
2026-04-27 14:38:00 +0200

    feat: lock screen orientation to portrait mode and update documentation second

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2h06c4ku0e"
+    "revision": "0.ei9qlp53kjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
